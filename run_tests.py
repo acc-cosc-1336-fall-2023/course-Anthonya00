cec25c8a5954ea7dea0168dfe5aad66e4984779c
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-09-16 18:44:47 +0000

    HW #2

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
-from tests.homework.b_in_proc_out import tests_in_proc_out 
 
-suite = unittest.TestLoader().loadTestsFromModule(test_decisions)
+from tests.homework.c_decisions import tests_decisions
+
+suite = unittest.TestLoader().loadTestsFromModule(tests_decisions)
 unittest.TextTestRunner(verbosity=2).run(suite)
-
-from test.examples.c_decisions import test_decisions 
+ 
--- tests/homework/c_decisions/tests_decisions.py
+++ tests/homework/c_decisions/tests_decisions.py
@@ -1,1 +1,18 @@
-#
+##A
+import unittest
+from src.homework.c_decisions.decisions import get_options_ratio
+from src.homework.c_decisions.decisions import get_faculty_rating
+
+class Test_Config(unittest.TestCase):
+    #B
+    def test_get_options_ratio(self):
+        self.assertAlmostEqual(get_options_ratio(5,20), 0.25)
+        self.assertAlmostEqual(get_options_ratio(10, 20), 0.5)
+    #C
+        self.assertEqual(get_faculty_rating(0.91), "Excellent")
+        self.assertEqual(get_faculty_rating(0.85), "Very Good")
+        self.assertEqual(get_faculty_rating(0.71), "Good")
+        self.assertEqual(get_faculty_rating(0.66), "Needs Improvement")
+        self.assertEqual(get_faculty_rating(0.45), "Unacceptable")
+if __name__ == "__main__":
+    unittest.main()    
