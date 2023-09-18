586a521132325063c87bbed1e04ddbfd7c317b92
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-09-18 23:59:55 +0000

    file loop

--- run_tests.py
+++ run_tests.py
@@ -4,3 +4,5 @@ from tests.homework.c_decisions import tests_decisions
 
 suite = unittest.TestLoader().loadTestsFromModule(tests_decisions)
 unittest.TextTestRunner(verbosity=2).run(suite) 
+
+fromtest.examples.d_repetition import test_repittion 
