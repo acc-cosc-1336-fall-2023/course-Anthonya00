0e482c04a82eebd4b1ecf84d9212db716f273323
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-09-20 23:05:36 +0000

    use sum of squares in mian

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,5 +1,9 @@
 import repetition
 
-
 #repetition.display_numbers(2)
-repetition.prompt_prompt_user()
+#repetition.prompt_prompt_user()
+#repetition.for_intro_loop()
+#repetition.for_intro_loop_strings() 
+
+result= repetition.for_sum_of_squares(5)
+print("sum of squares is: " + str(result))
