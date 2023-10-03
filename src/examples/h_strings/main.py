68a1ce8fa762323535daf24c605640389bacdd0e
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-10-03 00:08:52 +0000

    loop strings w while, for, n replace

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -1,9 +1,11 @@
 #main program
-letter = 'A'
-print(letter)
+import strings
 
+#strings.loop_string_w_while("python")
+#strings.loop_string_w_for("python")
+#strings.loop_string_w_special_for("python")
 lang = "python"
 
-print(lang)
-print(len(lang))
 print(lang[2])
+lang = lang.replace('y', 'Y')
+print(lang)
