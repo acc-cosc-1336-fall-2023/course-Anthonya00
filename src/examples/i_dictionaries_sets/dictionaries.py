df944c4befc89eed51e99d2a72b5c0161c33e99e
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-10-23 23:53:13 +0000

    find key w in, for key,value in items, test case

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,2 +1,7 @@
 def test_config():
     return True
+
+def is_key_in_dictionary(key,dictionary):
+    return key in dictionary
+
+    
--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,16 +1,15 @@
+import dictionaries
+
 #main program
 phonebook = {'Chris':'555-1111','katie':'555-2222','Joanne':'555-3333'}
-#print(phonebook)
-#print(phonebook['Chris'])
-#print(phonebook['katie'])
-#print(phonebook['cris'])
 
-for key in phonebook:
-    print(key ,phonebook[key])
+exists = dictionaries.is_key_in_dictionary('chris', phonebook)
 
-print("values only")
-for value in phonebook.values():
-    print(value)
-print("dictionary items")
-for item in phonebook.items():
-    print(item)
+print(exists)
+
+for key,value in phonebook.items():
+    print(key,value)
+
+    
+for name,number in phonebook.items():
+    print(name,value)
