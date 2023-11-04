f286aa968e8687df06bcd33506767581337416a8
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-11-04 00:18:58 +0000

    HW #8

--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -4,32 +4,39 @@ import unittest
 from src.homework.i_dictionaries_sets.dictionary import get_p_distance_matrix
 from src.homework.i_dictionaries_sets.dictionary import get_p_distance
 
+baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+
+basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])   
+
 class test_config(unittest.TestCase):
-    
-    def test_p_distance(self):
-        list1 = ['T','T','T','C','C','A','T','T','T','A']
-        list2 = ['G','A','T','T','C','A','T','T','T','C']
-        result = get_p_distance (list1,list2)
 
-        self.assertEqual(.4, result)
 
-    def test_get_p_distance_matrix(self):
+    def test_intersection(self):
+        self.assertEqual(baseball.intersection(basketball),set(["Carmen","Alicia"]))
 
-        list1 = [['T','T','T','C','C','A','T','T','T','A'],
-                ['G','A','T','T','C','A','T','T','T','C'],
-                ['T','T','T','C','C','A','T','T','T','T'],
-                ['G','T','T','C','C','A','T','T','T','A']]
+        print(baseball.intersection(basketball))
+
+    def test_union(self):
+        self.assertEqual(baseball.union(basketball),set(['Jodi', 'Aida', 'Alicia','Eva', 'Carmen', 'Sarah']))
         
-        result = get_p_distance_matrix(list1)
-        
-        self.assertEqual([
- [0.0, 0.4, 0.1, 0.1],
- [0.4, 0.0, 0.4, 0.3],
- [0.1, 0.4, 0.0, 0.2],
-[0.1, 0.3, 0.2, 0.0]
-],result)
-    
-                
-       
+        print(baseball.union(basketball))
+
+    def test_difference(self):
+        self.assertEqual(baseball.difference(basketball),set(['Jodi','Aida']))
+        print(baseball.difference(basketball))
+
+    def test_difference2(self):
+        self.assertEqual(basketball.difference(baseball),set(["Eva","Sarah"]))
+        self.assertEqual(baseball.difference(basketball),set(['Jodi','Aida']))
+
+        print(basketball.difference(baseball))
+        print(baseball.difference(basketball))
+
+    def test_symmetric(self):
+        self.assertEqual(basketball.symmetric_difference(baseball),set(["Eva","Sarah",'Jodi','Aida']))
+
+        print(basketball.symmetric_difference(baseball))
+
 
     
+        
