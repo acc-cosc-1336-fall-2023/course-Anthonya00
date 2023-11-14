ffa9a95b98a808f735729045d2a66aff08146da9
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-11-14 00:38:22 +0000

    account private method/function

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -1,14 +1,17 @@
 import bank_account,atm
 
 account = bank_account.BankAccount(50)
+account.__get_balance_from_db()
 
-print(account.get_balance())
+#print(account.get_balance())
 my_atm = atm.ATM(account)
 
-print(account.get_balance())
+menu.run_menu(my_atm)
 
-my_atm.make_deposit()
-my_atm.display_balance()
+#print(account.get_balance())
 
-my_atm.make_withdraw()
-my_atm.display_balance
+#my_atm.make_deposit()
+#my_atm.display_balance()
+
+#my_atm.make_withdraw()
+#my_atm.display_balance
